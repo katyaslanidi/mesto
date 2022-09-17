667fca19817ccf4b70db7473333a1f6794d0e1f5
Katya Aslanidi <grechkakat@yandex.ru>
2022-09-17 05:09:25 +0300

    added nojekyll

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,35 @@
-console.log('hello');
-//открыть-закрыть попап
 const overlayEl = document.querySelector('.overlay');
 const openPopup = document.querySelector('.profile__edit');
 const closePopup = document.querySelector('.overlay__button');
 
-openPopup.addEventListener('click', () => {
+const formElement = document.querySelector('.popup__form');
+const nameInput = document.querySelector('.popup__input_name');
+const jobInput = document.querySelector('.popup__input_job');
+const saveButton = document.querySelector('.popup__button');
+
+const nameProfile = document.querySelector('.profile__name');
+const jobProfile = document.querySelector('.profile__job');
+
+function OpenPopupListener() {
     overlayEl.classList.add('overlay_opened');
-})
-
-closePopup.addEventListener('click', () => {
-    overlayEl.classList.remove('overlay_opened');
-})
-
-
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__job');
-let saveButton = document.querySelector('.popup__button');
-
-let nameProfile = document.querySelector('.profile__name');
-let jobProfile = document.querySelector('.profile__job');
-
-//отображается имя и работа из профиля
-nameInput.value = nameProfile.textContent;
-jobInput.value = jobProfile.textContent;
-
+    nameInput.value = nameProfile.textContent;
+    jobInput.value = jobProfile.textContent;
+}
+function ClosePopupListener(){
+        overlayEl.classList.remove('overlay_opened');
+}
 //редактор профиля
 function formSubmitHandler(evt) {
     evt.preventDefault();
     nameProfile.textContent = document.getElementById("GET-name").value;
+    //я понимаю, что данное выражение эквивалентно записи: nameProfile.textContent = nameInput.value;
+    //но я не понимаю как мне по-другому сделать замену нового имени, вызвать тут функцию OpenPopupListener?
     jobProfile.textContent = document.getElementById("GET-job").value;
+    closePopup.addEventListener('click', ClosePopupListener);
+
 }
+
+openPopup.addEventListener('click', OpenPopupListener);
+closePopup.addEventListener('click', ClosePopupListener);
 formElement.addEventListener('submit', formSubmitHandler);
-saveButton.addEventListener('click', () => {
-    overlayEl.classList.remove('overlay_opened');
-})
+
