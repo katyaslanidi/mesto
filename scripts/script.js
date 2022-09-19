6d78321e2af26d6a67d8b4fcc67fc5bd85cccaab
Katya Aslanidi <grechkakat@yandex.ru>
2022-09-19 13:52:44 +0300

    it4

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,28 @@
 const overlayEl = document.querySelector('.overlay');
-const openPopup = document.querySelector('.profile__edit');
-const closePopup = document.querySelector('.overlay__button');
-
+const openPopupButton = document.querySelector('.profile__edit');
+const closePopupButton = document.querySelector('.overlay__button');
 const formElement = document.querySelector('.popup__form');
 const nameInput = document.querySelector('.popup__input_name');
 const jobInput = document.querySelector('.popup__input_job');
 const saveButton = document.querySelector('.popup__button');
-
 const nameProfile = document.querySelector('.profile__name');
 const jobProfile = document.querySelector('.profile__job');
 
-function openPopupListener() {
+function openPopup() {
     overlayEl.classList.add('overlay_opened');
     nameInput.value = nameProfile.textContent;
     jobInput.value = jobProfile.textContent;
 }
-function closePopupListener(){
+function closePopup() {
     overlayEl.classList.remove('overlay_opened');
 }
-
 function formSubmitHandler(evt) {
     evt.preventDefault();
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
-    closePopupListener();
+    closePopup();
 }
 
-openPopup.addEventListener('click', openPopupListener);
-closePopup.addEventListener('click', closePopupListener);
+openPopupButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
-
