2999c8e5b1c91d4874bae22c00fa10c22a4fd028
Katya Aslanidi <grechkakat@yandex.ru>
2022-09-30 11:17:42 +0300

    ПР5 начало

--- scripts/script.js
+++ scripts/script.js
@@ -9,20 +9,73 @@ const nameProfile = document.querySelector('.profile__name');
 const jobProfile = document.querySelector('.profile__job');
 
 function openPopup() {
-    overlayEl.classList.add('overlay_opened');
-    nameInput.value = nameProfile.textContent;
-    jobInput.value = jobProfile.textContent;
+  overlayEl.classList.add('overlay_opened');
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
 }
 function closePopup() {
-    overlayEl.classList.remove('overlay_opened');
+  overlayEl.classList.remove('overlay_opened');
 }
 function formSubmitHandler(evt) {
-    evt.preventDefault();
-    nameProfile.textContent = nameInput.value;
-    jobProfile.textContent = jobInput.value;
-    closePopup();
+  evt.preventDefault();
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
+  closePopup();
 }
 
 openPopupButton.addEventListener('click', openPopup);
 closePopupButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cards = document.querySelector('elements');
+
+function makeElementFromItem() {
+  let element = document.createElement('div');
+  element.classList.add('element');
+
+  let photo = document.createElement('img');
+  photo.classList.add('element__image');
+
+  let bottom = document.createElement('div');
+  bottom.classList.add('element__bottom');
+
+  let text = document.createElement('h2');
+  text.classList.add('element__text');
+
+  let like = document.createElement('button');
+  like.classList.add('element__like');
+}
+
+initialCards.forEach(item => {
+  // создать карточку
+  element = makeElementFromItem(item);
+  // установить эту карточку куда-то в DOM-дереве
+  document.body.appendChild(cards);
+});
